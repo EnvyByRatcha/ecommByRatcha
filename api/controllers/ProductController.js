5f6867a0aae1c1a56b6e089b6fe1500cd7ddf3b7
@EnvyByRatcha <ratchanon.mskcr@gmail.com>
2024-07-18 15:41:50 +0700

    add update product and import excel

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const fileUpload = require("express-fileupload");
+const excelJs = require("exceljs");
 
 dotenv.config();
 
-app.post("/create", async (req, res) => {
+app.use(fileUpload());
+
+app.post("/createProduct", async (req, res) => {
   try {
     const result = await prisma.product.create({
       data: req.body,
@@ -48,6 +51,108 @@ app.delete("/removeProduct/:id", async (req, res) => {
     });
 
     res.send({ message: "success" });
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+});
+
+app.put("/updateProduct", async (req, res) => {
+  try {
+    const fs = require("fs");
+    const oldData = await prisma.product.findFirst({
+      where: {
+        id: parseInt(req.body.id),
+      },
+    });
+
+    if (oldData.img != "") {
+      const imagePath = "./uploads/" + oldData.img;
+
+      if (fs.existsSync(imagePath)) {
+        await fs.unlinkSync(imagePath);
+      }
+    }
+
+    await prisma.product.update({
+      data: req.body,
+      where: {
+        id: parseInt(req.body.id),
+      },
+    });
+
+    res.send({ message: "success" });
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+});
+
+app.post("/upload", async (req, res) => {
+  try {
+    if (req.files != undefined) {
+      if (req.files.img != undefined) {
+        const img = req.files.img;
+        const fs = require("fs");
+        const myDate = new Date();
+        const y = myDate.getFullYear();
+        const m = myDate.getMonth();
+        const d = myDate.getDate();
+        const h = myDate.getHours();
+        const min = myDate.getMinutes();
+        const s = myDate.getSeconds();
+        const ms = myDate.getMilliseconds();
+
+        const arrFileName = img.name.split(".");
+        const ext = arrFileName[arrFileName.length - 1];
+
+        const newName = `${y}${m}${d}${h}${min}${s}${ms}.${ext}`;
+
+        img.mv("./uploads/" + newName, (err) => {
+          if (err) throw err;
+
+          res.send({ newName: newName });
+        });
+      }
+    } else {
+      res.status(501).send("notImpremented");
+    }
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+});
+
+app.post("/uploadFromExcel", (req, res) => {
+  try {
+    const fileExcel = req.files.fileExcel;
+    fileExcel.mv("./uploads/" + fileExcel.name, async (err) => {
+      if (err) throw err;
+
+      const workbook = new excelJs.Workbook();
+      await workbook.xlsx.readFile("./uploads/" + fileExcel.name);
+
+      const ws = workbook.getWorksheet(1);
+
+      for (let i = 2; i <= ws.rowCount; i++) {
+        const name = ws.getRow(i).getCell(1).value ?? "";
+        const cost = ws.getRow(i).getCell(2).value ?? 0;
+        const price = ws.getRow(i).getCell(3).value ?? 0;
+
+        if (name != "" && cost >= 0 && price >= 0) {
+          await prisma.product.create({
+            data: {
+              name: name,
+              cost: cost,
+              price: price,
+              img: "",
+            },
+          });
+        }
+      }
+
+      const fs = require("fs");
+      await fs.unlinkSync("./uploads/" + fileExcel.name);
+
+      res.send({ message: "success" });
+    });
   } catch (e) {
     res.status(500).send({ message: e.message });
   }
--- app/backoffice/src/pages/backoffice/Product.js
+++ app/backoffice/src/pages/backoffice/Product.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import BackOffice from "../../components/BackOffice";
 import Modal from "../../components/Modal";
 import Swal from "sweetalert2";
@@ -8,6 +8,10 @@ import config from "../../config";
 function Product() {
   const [product, setProduct] = useState({});
   const [products, setProducts] = useState([]);
+  const [img, setImg] = useState({});
+  const refImg = useRef();
+  const refExcel = useRef();
+  const [fileExcel, setFileExcel] = useState({});
 
   const fetchData = async () => {
     try {
@@ -32,15 +36,24 @@ function Product() {
 
   const handleAddProduct = async () => {
     try {
-      product.img = "";
+      product.img = await handleUpload();
       product.cost = parseInt(product.cost);
       product.price = parseInt(product.price);
 
-      const res = await axios.post(
-        config.apiPath + "/product/create",
-        product,
-        config.headers
-      );
+      let res;
+      if (product.id === undefined) {
+        res = await axios.post(
+          config.apiPath + "/product/createProduct",
+          product,
+          config.headers
+        );
+      } else {
+        res = await axios.put(
+          config.apiPath + "/product/updateProduct",
+          product,
+          config.headers
+        );
+      }
 
       if (res.data.message === "success") {
         Swal.fire({
@@ -53,6 +66,7 @@ function Product() {
 
       document.getElementById("modalProduct_btnClose").click();
       fetchData();
+      setProduct({ ...product, id: undefined });
     } catch (e) {
       Swal.fire({
         title: "error",
@@ -68,6 +82,9 @@ function Product() {
       cost: "",
       price: "",
     });
+
+    setImg({});
+    refImg.current.value = "";
   };
 
   const handleRemove = async (item) => {
@@ -105,6 +122,107 @@ function Product() {
     }
   };
 
+  const selectedFile = (inputFile) => {
+    if (inputFile !== undefined) {
+      if (inputFile.length > 0) {
+        setImg(inputFile[0]);
+      }
+    }
+  };
+
+  const handleUpload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("img", img);
+
+      const res = await axios.post(
+        config.apiPath + "/product/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+
+      if (res.data.newName != undefined) {
+        return res.data.newName;
+      }
+    } catch (e) {
+      Swal.fire({
+        title: "error",
+        text: e.message,
+        icon: "error",
+      });
+
+      return "";
+    }
+  };
+
+  const showImg = (item) => {
+    if (item.img !== "") {
+      return (
+        <img
+          alt=""
+          className="img-thumbnail"
+          src={config.apiPath + "/uploads/" + item.img}
+          width="150"
+          height="150"
+        />
+      );
+    }
+  };
+
+  const selectedFileExcel = (fileInput) => {
+    if (fileInput !== undefined) {
+      if (fileInput.length > 0) {
+        setFileExcel(fileInput[0]);
+      }
+    }
+  };
+
+  const handleUploadExcel = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("fileExcel", fileExcel);
+
+      const res = await axios.post(
+        config.apiPath + "/product/uploadFromExcel",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+
+      if (res.data.message === "success") {
+        Swal.fire({
+          title: "อัปโหลดไฟล์",
+          text: "อัปโหลดไฟล์แล้ว",
+          icon: "success",
+          itmer: 2000,
+        });
+
+        fetchData();
+        document.getElementById("modalExcel_btnClose").click();
+      }
+    } catch (e) {
+      Swal.fire({
+        title: "error",
+        text: e.message,
+        icon: "error",
+      });
+    }
+  };
+
+  const clearFormExcel = () => {
+    refExcel.current.value = "";
+    setFileExcel(null);
+  };
+
   return (
     <BackOffice>
       <div>
@@ -112,16 +230,25 @@ function Product() {
       </div>
       <button
         onClick={clearForm}
-        className="btn btn-primary"
+        className="btn btn-primary mr-2"
         data-toggle="modal"
         data-target="#modalProduct"
       >
         <i className="fa fa-plus mr-2"></i>เพิ่มสินค้า
       </button>
+      <button
+        onClick={clearFormExcel}
+        className="btn btn-success"
+        data-toggle="modal"
+        data-target="#modalExcel"
+      >
+        <i className="fa fa-arrow-down mr-2"></i>Import from Excel
+      </button>
 
       <table className="table table-bordered table-striped mt-2">
         <thead>
           <tr>
+            <th width="150px">ภาพสินค้า</th>
             <th>ชื่อสินค้า</th>
             <th className="text-right">ราคาต้นทุน</th>
             <th className="text-right">ราคาขาย</th>
@@ -132,11 +259,17 @@ function Product() {
           {products.length > 0 ? (
             products.map((item) => (
               <tr key={item.id}>
+                <td>{showImg(item)}</td>
                 <td>{item.name}</td>
                 <td className="text-right">{item.cost}</td>
                 <td className="text-right">{item.price}</td>
                 <td className="text-center">
-                  <button className="btn btn-primary mr-2">
+                  <button
+                    data-toggle="modal"
+                    data-target="#modalProduct"
+                    onClick={(e) => setProduct(item)}
+                    className="btn btn-primary mr-2"
+                  >
                     <i className="fa fa-edit"></i>
                   </button>
                   <button
@@ -185,18 +318,36 @@ function Product() {
         <div className="mt-2">
           <div>ภาพสินค้า</div>
           <input
-            onChange={(e) => setProduct({ ...product, img: e.target.value })}
+            onChange={(e) => selectedFile(e.target.files)}
+            ref={refImg}
             type="file"
             id="inputGroupFile02"
             className="form-control"
           />
         </div>
 
+        <div className="mt-2">
+          <div>{showImg(product)}</div>
+        </div>
+
         <div className="mt-5">
           <button onClick={handleAddProduct} className="btn btn-primary">
-            <i className="fa fa-check"></i>เพื่มสินค้า
+            <i className="fa fa-check mr-2"></i>เพื่มสินค้า
           </button>
         </div>
+      </Modal>
+
+      <Modal id="modalExcel" title="เลือกไฟล์อัพโหลด">
+        <div>เลือกไฟล์</div>
+        <input
+          ref={refExcel}
+          onChange={(e) => selectedFileExcel(e.target.files)}
+          className="form-control"
+          type="file"
+        />
+        <button onClick={handleUploadExcel} className="btn btn-primary mt-5">
+          <i className="fa fa-check mr-2"></i>บันทึก
+        </button>
       </Modal>
     </BackOffice>
   );
